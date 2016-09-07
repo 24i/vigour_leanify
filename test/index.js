33df90873e32a1832c46c7ff4918f7bd4bc6d225
Mustafa Dokumaci <mustafa@foraa.com>
2016-09-07 16:58:36 +0200

    start testing

--- test/index.js
+++ test/index.js
@@ -1,1 +1,33 @@
 'use strict'
+
+const test = require('tape')
+const leanify = require('../lib/index')
+
+test('No optimization', function (t) {
+  const code = 'const testVar = 5;\nvar otherVar = 7;\nif (otherVar == \'dumb\') {\n  console.log(otherVar);\n}'
+
+  var buffer = ''
+
+  leanify('test.js')
+    .on('data', d => { buffer += d })
+    .on('end', function () {
+      t.equal(buffer, code, 'code is not changed')
+      t.end()
+    })
+    .end(code)
+})
+
+test('identifier optimization', function (t) {
+  const oldcode = 'const testVar = 5;\nif (testVar === \'dumb\') {\n  console.log(testVar);\n}'
+  const newCode = 'const testVar = 5;'
+
+  var buffer = ''
+
+  leanify('test.js')
+    .on('data', d => { buffer += d })
+    .on('end', function () {
+      t.equal(buffer, newCode, 'if removed')
+      t.end()
+    })
+    .end(oldcode)
+})
